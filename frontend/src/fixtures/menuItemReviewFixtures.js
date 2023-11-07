2fc3e415a537e3e0b60fb45b60800ba3c1e89b93
lukewli <lukewli@ucsb.edu>
2023-11-07 13:59:20 -0800

    lli - Create menuItemReviewFixtures file

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,43 @@
+const menuItemReviewFixtures = {
+  oneMenuItemReview: [
+    {
+      id: 1,
+      itemId: 100,
+      reviewerEmail: 'Luke1@ucsb.edu',
+      stars: 3,
+      dateReviewed: '2022-01-02T12:00:00',
+      comments: 'Some comments about item 100',
+    },
+  ],
+
+  threeMenuItemReviews: [
+    {
+      id: 2,
+      itemId: 101,
+      reviewerEmail: 'Luke2@ucsb.edu',
+      stars: 1,
+      dateReviewed: '2023-01-02T12:00:00',
+      comments: 'Some comments about item 101',
+    },
+
+    {
+      id: 3,
+      itemId: 102,
+      reviewerEmail: 'Luke3@ucsb.edu',
+      stars: 4,
+      dateReviewed: '2022-02-02T12:00:00',
+      comments: 'Some comments about item 102',
+    },
+
+    {
+      id: 4,
+      itemId: 103,
+      reviewerEmail: 'Luke4@ucsb.edu',
+      stars: 5,
+      dateReviewed: '2022-05-02T12:00:00',
+      comments: 'Some comments about item 103',
+    },
+  ],
+};
+
+export { restaurantFixtures };
