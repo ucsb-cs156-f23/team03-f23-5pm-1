b9baade3bddc7e6d4de4e3b2c017629e47ac2e1f
Angel G <angel@169-231-116-144.wireless.ucsb.edu>
2023-11-08 17:02:55 -0800

    added fixtures file for dining commons menu item

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,34 @@
+const diningCommonsMenuItemFixtures = {
+    oneMenuItem:
+    [
+      {
+       "diningCommonsCode": "carillo",
+        "name": "cheeseburger",
+        "station": "entree"      
+      }
+    ],
+
+    threeRestaurants:
+    [
+        {
+            "diningCommonsCode": "portola",
+             "name": "pizza",
+             "station": "special entree",      
+        },
+
+        {
+            "diningCommonsCode": "carillo",
+             "name": "cheeseburger",
+             "station": "entree"  
+        },
+
+        {
+            "diningCommonsCode": "ortega",
+             "name": "pasta",
+             "station": "entree"      
+        },
+        
+    ]
+};
+
+export { restaurantFixtures };
