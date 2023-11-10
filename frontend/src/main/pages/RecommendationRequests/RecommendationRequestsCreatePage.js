28c0d31e8eb3fdddd291c764bdde3876065593d3
Yashasvi-Vangala <55556296+Yashasvi-Vangala@users.noreply.github.com>
2023-11-09 17:37:14 -0800

    added page files to render in storybook

---
+++ frontend/src/main/pages/RecommendationRequests/RecommendationRequestsCreatePage.js
@@ -0,0 +1,53 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestForm from "main/components/RecommendationRequests/RecommendationRequestForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
+
+export default function RecommendationRequestsCreatePage({storybook=false}) {
+
+  const objectToAxiosParams = (recommendationRequest) => ({
+    url: "/api/recommendationrequests/post",
+    method: "POST",
+    params: {
+      requesterEmail: recommendationRequest.requesterEmail,
+      professorEmail: recommendationRequest.professorEmail,
+      explanation: recommendationRequest.explanation,
+      dateNeeded: recommendationRequest.dateNeeded,
+      dateRequested: recommendationRequest.dateRequested,
+      done: recommendationRequest.done,
+    }
+  });
+
+  const onSuccess = (recommendationRequest) => {
+    toast(`New recommendationRequest Created - id: ${recommendationRequest.id} Your Email: ${recommendationRequest.requesterEmail}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/recommendationrequests/all"]
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/recommendationrequests" />
+  }
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Create New RecommendationRequest</h1>
+
+        <RecommendationRequestForm submitAction={onSubmit} />
+
+      </div>
+    </BasicLayout>
+  )
+}
